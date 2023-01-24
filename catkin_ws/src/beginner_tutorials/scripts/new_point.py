0b5845ac44449bc859cef154c24ab9f8e93c9a9d
IsaacHarper01 <isaac.anibal.pk@gmail.com>
2023-01-23 18:15:02 -0600

    solved the tf problem, still send wrong data when the robot turns

--- catkin_ws/src/beginner_tutorials/scripts/new_point.py
+++ catkin_ws/src/beginner_tutorials/scripts/new_point.py
@@ -7,20 +7,24 @@ from geometry_msgs.msg import PointStamped
 point_get = PointStamped()
 
 def husky_to_world(point):
+    #global point_get
     pub_point = rospy.Publisher('new_point',PointStamped,queue_size=1)
     new_point = PointStamped()
     listener = tf.TransformListener()
-    listener.waitForTransform("rslidar", "odom", rospy.Time.now(), rospy.Duration(4.0))
-    while not rospy.is_shutdown():
-            now = rospy.Time.now()
-            listener.waitForTransform("rslidar", "odom", now, rospy.Duration(4.0))
-            new_point = listener.transformPoint('odom',point)
-            pub_point.publish(new_point)
+    listener.waitForTransform("rslidar", "odom", rospy.Time(), rospy.Duration(4.0))
+    now = rospy.Time.now()
+    listener.waitForTransform("rslidar", "odom", now, rospy.Duration(4.0))
+    new_point = listener.transformPoint('odom',point)
+    pub_point.publish(new_point)
 
 def call(msg):
     global point_get
     point_get = msg
+    husky_to_world(point_get)
 
 rospy.init_node('tf_convertion')
-sub = rospy.Subscriber("/point_scan", PointStamped,call)
-husky_to_world(point_get)
+sub = rospy.Subscriber("/point_scan", PointStamped, call)
+rospy.spin() 
+    
+    
+    
--- catkin_ws/src/beginner_tutorials/scripts/turtle_tf_broadcaster.py
+++ catkin_ws/src/beginner_tutorials/scripts/turtle_tf_broadcaster.py
@@ -3,9 +3,11 @@ import rospy
 import tf
 
 def pub_tf():
-    rospy.init_node('turtle_tf_broadcaster')
     br = tf.TransformBroadcaster()
     while not rospy.is_shutdown():
         br.sendTransform((2.0, 3.0, 0.0),(0.0, 0.0, 0.0, 1.0),rospy.Time.now(),'husky','world')        
 
+
+rospy.init_node('turtle_tf_broadcaster')
 pub_tf()
+rospy.spin()

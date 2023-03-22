24e205f51d1716654acd0e16cf9e78ba4d2c2de8
IsaacHarper01 <isaac.anibal.pk@gmail.com>
2023-03-21 22:07:15 -0600

    vision

--- catkin_ws/src/beginner_tutorials/scripts/alling_lidar.py
+++ catkin_ws/src/beginner_tutorials/scripts/alling_lidar.py
@@ -2,8 +2,9 @@
 
 from __future__ import division
 import rospy 
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan, PointCloud2
 import statistics 
+import sensor_msgs.point_cloud2 as pc2
 
 class robot:
     def __init__(self):
@@ -33,7 +34,32 @@ class robot:
             print(error_line)
             return True
 
+class LidarConverter:
+    def __init__(self):
+        self.cloud_sub = rospy.Subscriber('/camera/depth/points', PointCloud2, self.convert_cloud)
+        self.lidar_pub = rospy.Publisher('/lidar_topic', LaserScan, queue_size=10)
+
+    def convert_cloud(self, msg):
+        scan = LaserScan()
+        scan.header = msg.header
+        scan.angle_min = -3.14159
+        scan.angle_max = 3.14159
+        scan.angle_increment = 0.01745
+        scan.time_increment = 0.0
+        scan.scan_time = 0.1
+        scan.range_min = 0.0
+        scan.range_max = 100.0
+        scan.ranges = []
+        scan.intensities = []
+
+        for point in pc2.read_points(msg, skip_nans=True):
+            scan.ranges.append(point[2])
+            scan.intensities.append(point[3])
+
+        self.lidar_pub.publish(scan)
+    
 rospy.init_node("alling")
-robot = robot()
-print(robot.comp())
+lc = LidarConverter()
+#robot = robot()
+#print(robot.comp())
 rospy.spin()
